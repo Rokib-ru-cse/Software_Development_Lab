937e2033ab0efc7a8ce08137654e27922e62ae4e
rokib <rokibahmed.ru.cse@gmail.com>
2018-04-24 18:19:52 +0600

    adding add function in the main function

--- todo_list.cpp.cpp
+++ todo_list.cpp.cpp
@@ -8,7 +8,19 @@ struct todo
 {
 	char time[250], task[250];
 };
+void addTask()
+{
+	ofstream fileWrite("tasks.txt", ios::app | ios::binary);
+	struct todo myTask;
+	cout << "Insert task: ";
+	cin >> myTask.task;
+	cout << "Insert time: ";
+	cin >> myTask.time;
 
+	fileWrite.write((char *) &myTask, sizeof(myTask));
+	cout << "Added!\n";
+	fileWrite.close();
+}
 
 
 
@@ -28,7 +40,14 @@ int main()
 
 		char option;
 		cin >> option;
-
+        switch(option)
+		{
+		    case('a'):
+				addTask();
+				break;
+			default:
+				cout << "Invalid option!\n";
+        }
 
 	}
 	return 0;
