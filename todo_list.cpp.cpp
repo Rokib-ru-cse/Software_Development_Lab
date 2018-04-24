16c3a041bf13dc085c2e26d123b3a87dfbf30a77
rokib <rokibahmed.ru.cse@gmail.com>
2018-04-24 18:14:44 +0600

    add menu in the main function

---
+++ todo_list.cpp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+struct todo
+{
+	char time[250], task[250];
+};
+
+
+
+
+
+
+
+int main()
+{
+
+
+	while(true)
+	{
+		cout << "a) Add Tasks\n";
+		cout << "b) Delete Tasks\n";
+		cout << "c) View Tasks\n";
+		cout << "d) Exit\n";
+
+		char option;
+		cin >> option;
+
+
+	}
+	return 0;
+}
